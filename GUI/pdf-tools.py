e74d3dd64e2e1ffe4aade4d183697301bf152130
rusl2019 <muhammad.ruslan14@gmail.com>
2021-02-10 23:11:42 +0700

    mengganti ImageTk menjadi PhotoImage

--- GUI/pdf-tools.py
+++ GUI/pdf-tools.py
@@ -1,39 +1,56 @@
 #!/usr/bin/env python3
 
-import os.path
 import PyPDF2
 import tkinter as tk
 from tkinter import PhotoImage, filedialog, messagebox
-from tkinter.ttk import *
-from PIL import Image, ImageTk
 import webbrowser as wb
+import sys
 
 def getFile():
     global file_input
-    file_input = filedialog.askopenfilenames()
+    file_input = filedialog.askopenfilenames(
+        defaultextension='.pdf',
+        filetypes=[('file pdf', '*.pdf'), ('file gambar', '*.png')],
+        title="Pilih file"
+    )
+    file_input = list(file_input)
+
 
 
 def pdf_merger():
     global file_input
+    if file_input[0].split('.')[-1].lower() != 'pdf':
+        messagebox.showerror(
+            title="Error!!!", 
+            message="File masukan bukan file PDF"
+        )
+        sys.exit(1)
     merge_pdfs = PyPDF2.PdfFileMerger()
     for pdf in file_input:
         merge_pdfs.append(open(pdf, 'rb'))
-    file_output = filedialog.asksaveasfilename(defaultextension='.pdf')
+    file_output = filedialog.asksaveasfilename(
+        defaultextension='.pdf', 
+        filetypes=[("PDF files", '*.pdf')],
+    )
     merge_pdfs.write(open(file_output, 'wb'))
 
 
 def pdf_splitter():
     global file_input
+    if file_input[0].split('.')[-1].lower() != 'pdf':
+        messagebox.showerror(
+            title="Error!!!", 
+            message="File masukan bukan file PDF"
+        )
+        sys.exit(1)
+    path = filedialog.asksaveasfilename()
     file_input = file_input[0]
-    fname = os.path.splitext(os.path.basename(file_input))[0]
     pdf = PyPDF2.PdfFileReader(file_input)
     for page in range(pdf.getNumPages()):
         pdf_writer = PyPDF2.PdfFileWriter()
         pdf_writer.addPage(pdf.getPage(page))
-        output_filename = '{}_page_{}.pdf'.format(
-            fname, page+1
-        )
-        with open(output_filename, 'wb') as out:
+        output_filename = '_page_{}.pdf'.format(page+1)
+        with open("{}{}".format(path, output_filename), 'wb') as out:
             pdf_writer.write(out)
 
 
@@ -50,17 +67,16 @@ def about():
 if __name__ == "__main__":
     # --- init ---
     root = tk.Tk()
-    root.wm_title("PDF Tool")
+    root.wm_title("PDF Tools")
     version = "1.0.0"
     # --- Window ---
     canvas = tk.Canvas(root, width=400, height=400,
                        relief="raised", background="white")
     canvas.pack()
-    p1 = PhotoImage(file="../img/favicon.png")
-    root.iconphoto(False,p1)
+    icon = PhotoImage(file="../img/favicon.png")
+    root.iconphoto(False,icon)
     # --- logo ---
-    logo = Image.open("../img/logo2.png")
-    logo = ImageTk.PhotoImage(logo)
+    logo = PhotoImage(file="../img/logo2.png")
     logo_label = tk.Label(image=logo, background="white")
     logo_label.image = logo
     canvas.create_window(200, 75, window=logo_label)
@@ -73,19 +89,19 @@ if __name__ == "__main__":
     root.config(menu=menubar)
     # --- tombol select file ---
     browseButton = tk.Button(
-        text="Select File", command=getFile, font=('helvetica', 12, 'bold'),
+        text="Pilih file", command=getFile, font=('helvetica', 12, 'bold'),
         background="#9D3BE1", foreground="#79FFA0"
     )
     canvas.create_window(200, 150, window=browseButton)
     # --- pdf merger ---
     pdfMerger = tk.Button(
-        text='Merger PDF', command=pdf_merger, font=('helvetica', 12, 'bold'),
+        text='Gabungkan file PDF', command=pdf_merger, font=('helvetica', 12, 'bold'),
         background="#9D3BE1", foreground="#79FFA0"
     )
     canvas.create_window(200, 200, window=pdfMerger)
     # --- pdf splitter ---
     pdfSplitter = tk.Button(
-        text='Splitter PDF', command=pdf_splitter, font=('helvetica', 12, 'bold'),
+        text='Membagi file PDF', command=pdf_splitter, font=('helvetica', 12, 'bold'),
         background="#9D3BE1", foreground="#79FFA0"
     )
     canvas.create_window(200, 250, window=pdfSplitter)
